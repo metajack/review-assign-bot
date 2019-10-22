c215773be7d682cad2947c218a95d333060c97b3
Jack Moffitt <metajack@fb.com>
2019-10-22 17:12:56 -0500

    Add support for draft PRs
    This skips assignment on draft PRs and handles the draft PR becoming a regular PR.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ interface AppConfig {
 }
 
 export = (app: Application) => {
-  app.on('pull_request.opened', async (context: Context) => {
+  app.on(['pull_request.opened', 'pull_request.ready_for_review'], async (context: Context) => {
     const config: AppConfig | null = await context.config<AppConfig | null>("review-assign-bot.yml");
     if (!config) {
       throw new Error('configuration failed to load');
@@ -17,6 +17,12 @@ export = (app: Application) => {
       context.repo({number: pr_number}));
     if (review_requests.data.users && review_requests.data.users.length > 0) {
       context.log("skipping review assignment due to existing review request");
+      return;
+    }
+
+    // Do nothing if this is a draft PR
+    if (context.payload.pull_request.draft) {
+      context.log("skipping review assignment due to draft status");
       return;
     }
 
